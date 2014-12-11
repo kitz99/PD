1301b5ff1f59138a0e1fc58d0633e3d9b35fd915
Bogdan <kitz_99@yahoo.com>
2014-12-11 11:45:58 +0200

    lab 8 si lab 10

---
+++ LAB8/lab8/mem.ml
@@ -0,0 +1,17 @@
+
+let rec lookup x = function
+  | [] -> raise Not_found
+  | (y,v)::_ when x=y -> v
+  | _::s -> lookup x s
+
+let rec update (x,n) = function
+  | [] -> raise Not_found
+  | (y,v)::s  when x=y -> ((x,n)::s)
+  | h::t -> h::(update (x,n) t)
+
+let string_of_mem = 
+  let string_of_pair (x,v) = x ^ " |-> " ^ ImpAST.string_of_expr v in
+  let rec string_of_mem str = function
+  | [] -> str
+  | p::t -> string_of_mem (str ^ "; " ^ string_of_pair p) t
+  in function [] -> "" | p::t -> string_of_mem (string_of_pair p) t
